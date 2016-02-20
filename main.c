1bc1416eb53ba57ea3bbc5f789e17c9ffe8101c2
Georgi <Georgi@Georgi-PC>
2016-02-20 19:42:36 +0200

    20, Excercise 1-8 Write a program to count blanks, tabs, and newlines

--- main.c
+++ main.c
@@ -1,8 +1,19 @@
-//17, Exercise 1-6. Verify that the expression qetchar () I= EOFis 0 or 1.
+//20, Excercise 1-8 Write a program to count blanks, tabs, and newlines
 
 #include <stdio.h>
-
+//Exercise 1..8. Write a program to count blanks, tabs, and newlines
 int main() {
-    printf("%d", EOF);
+    int c;
+    int white_space = 0, tab = 0, new_line;
+    
+    while ( ((c = getchar()) != EOF )) {
+        if (c == ' ')
+            ++white_space;
+        if (c == '\t')
+            ++tab;
+        if (c == '\n')
+            ++new_line;
+    }
+    printf("\n%d white spaces; %d tabs; %d new lines;\n", white_space, tab, new_line);
     return 0;
 }
